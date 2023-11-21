84f364c3a8607ecc565ff25006be55da326ec989
henrytran1604 <quanghuy1604nd@gmail.com>
2023-11-21 21:08:15 +0700

    21/11/2023

--- helper.py
+++ helper.py
@@ -1,11 +1,17 @@
 from colorama import init, Style, Fore
 init()
 
+username ='>>> TÔI   : '
+chatbot = ">>>CHATBOT: "
+
 def user_print(text):
-    print(f'\033[1A{Fore.CYAN}{text}{Style.RESET_ALL}\033[K')
+    print(f'\033[1A{Fore.CYAN}{username}{text}{Style.RESET_ALL}\033[K')
 
 def chatbot_print(str):
+    print(f'{Fore.GREEN}{chatbot}{str}{Style.RESET_ALL}')
+
+def chatbot_print2(str):
     print(f'{Fore.GREEN}{str}{Style.RESET_ALL}')
 
 def options_print(str):
-    print(f'{Fore.MAGENTA}{str}{Style.RESET_ALL}')
+    print(f'{Fore.MAGENTA}{" " * 9}{str}{Style.RESET_ALL}')
--- main.py
+++ main.py
@@ -36,13 +36,13 @@ class Main:
 
     def gender_question(self):
         chatbot_print(
-            f"{self.chatbot} Con của bạn là bé trai hay bé gái? (1 - bé trai, 0 - bé gái)"
+            "Con của bạn là bé trai hay bé gái? (1 - bé trai, 0 - bé gái)"
         )
         self.gender = int(input())
-        user_print(f'{self.username} Con tôi là {"con trai" if self.gender == 1 else "con gái"}')
+        user_print(f'Con tôi là {"con trai" if self.gender == 1 else "con gái"}')
 
     def age_question(self):
-        question = f"{self.chatbot} Con bạn bao nhiêu tháng tuổi? (Nhập 1 số từ 1 đến 24)"
+        question = "Con bạn bao nhiêu tháng tuổi? (Nhập 1 số từ 1 đến 24)"
         chatbot_print(question)
         self.age = int(input())
         if 0 <= self.age < 1:
@@ -61,19 +61,19 @@ class Main:
             period = "P7"
         else:
             period = "P8"
-        user_print(f"{self.username} Con tôi {self.age} tháng tuổi, [{self.facts[period]}]")
+        user_print(f"Con tôi {self.age} tháng tuổi, [{self.facts[period]}]")
 
         self.current_problems.append(period)
 
     def height_weight_question(self):
-        height_question = f"{self.chatbot} Con của bạn có chiều cao là bao nhiêu cm?"
+        height_question = "Con của bạn có chiều cao là bao nhiêu cm?"
         chatbot_print(height_question)
         self.height = float(input())
-        user_print(f"{self.username} Con tôi cao {self.height} cm")
-        weight_question = f"{self.chatbot} Con của bạn có cân nặng là bao nhiêu (kg)?"
+        user_print(f"Con tôi cao {self.height} cm")
+        weight_question = "Con của bạn có cân nặng là bao nhiêu (kg)?"
         chatbot_print(weight_question)
         self.weight = float(input())
-        user_print(f"{self.username} Con tôi nặng {self.weight} kg")
+        user_print(f"Con tôi nặng {self.weight} kg")
         self.current_problems.append(
             classify(self.gender, self.age, self.height, self.weight)
         )
@@ -81,48 +81,48 @@ class Main:
     def __ask(self, question_keys):
         cnt = 0
         while cnt < len(question_keys):
-            chatbot_print(f"{self.chatbot} Con của bạn có dấu hiệu nào trong các đặc điểm sau hay không?")
+            chatbot_print("Con của bạn có dấu hiệu nào trong các đặc điểm sau hay không?")
             for i, key in enumerate(question_keys):
                 if key not in self.current_problems:
-                    options_print(f"      \t{i + 1}: {self.facts[key]}")
-            options_print("      \t0. Con tôi không có triệu chứng nào ở trên")
+                    options_print(f"{i + 1}: {self.facts[key]}")
+            options_print("0. Con tôi không có triệu chứng nào ở trên")
             ans = int(input())
             if ans == 0:
-                user_print(f'{self.username} Con tôi không có dấu hiệu nào kể trên')
+                user_print(f'Con tôi không có dấu hiệu nào kể trên')
                 break
-            user_print(f"{self.username} Con tôi có dấu hiệu: {self.facts[question_keys[ans - 1]]}")
+            user_print(f"Con tôi có dấu hiệu: {self.facts[question_keys[ans - 1]]}")
 
             self.current_problems.append(question_keys[ans - 1])
             cnt += 1
 
     def health_question(self):
-        chatbot_print(f"{self.chatbot} Chúng tôi muốn biết tình trạng về **SỨC KHỎE** hiện tại của con bạn")
+        chatbot_print("Chúng tôi muốn biết tình trạng về **SỨC KHỎE** hiện tại của con bạn")
         health_symptom_keys = ["SY2", "SY11", "SY43"]
         self.__ask(health_symptom_keys)
 
     def skin_question(self):
-        chatbot_print(f"{self.chatbot} Chúng tôi muốn biết tình trạng về **DA** hiện tại của con bạn")
+        chatbot_print("Chúng tôi muốn biết tình trạng về **DA** hiện tại của con bạn")
         skin_symptom_keys = ["SY5", "SY6", "SY7", "SY41"]
         self.__ask(skin_symptom_keys)
 
     def sleep_question(self):
-        chatbot_print(f"{self.chatbot} Chúng tôi muốn biết tình trạng về **GIẤC NGỦ** hiện tại của con bạn")
+        chatbot_print("Chúng tôi muốn biết tình trạng về **GIẤC NGỦ** hiện tại của con bạn")
         sleep_symptom_keys = ["SY13", "SY14"]
         self.__ask(sleep_symptom_keys)
 
     def digest_question(self):
-        chatbot_print(f"{self.chatbot} Chúng tôi muốn biết tình trạng về **TIÊU HÓA** hiện tại của con bạn")
+        chatbot_print("Chúng tôi muốn biết tình trạng về **TIÊU HÓA** hiện tại của con bạn")
         digest_symptom_keys = ["SY18", "SY33", "SY40"]
         self.__ask(digest_symptom_keys)
 
     def respiratory_question(self):
-        chatbot_print(f"{self.chatbot} Chúng tôi muốn biết tình trạng về **TIÊU HÓA** hiện tại của con bạn")
+        chatbot_print("Chúng tôi muốn biết tình trạng về **TIÊU HÓA** hiện tại của con bạn")
         respiratory_symp_keys = ["SY19", "SY29"]
         self.__ask(respiratory_symp_keys)
 
     def vision_question(self):
-        chatbot_print(f"{self.chatbot} Chúng tôi muốn biết tình trạng về **THỊ GIÁC** hiện tại của con bạn")
-        vision_symp_keys = ["SY28", "SY28", "SY30", "SY15"]
+        chatbot_print("Chúng tôi muốn biết tình trạng về **THỊ GIÁC** hiện tại của con bạn")
+        vision_symp_keys = ["SY27", "SY28", "SY30", "SY15"]
         self.__ask(vision_symp_keys)
 
     def predict(self):
@@ -137,10 +137,22 @@ class Main:
         self.respiratory_question()
         self.vision_question()
         fc = ForwardChaining()
-        predict_reasons = [x for x in fc.forward_chaining(self.current_problems)[2] if x[:2] == 'RS']
-        advices = [x for x in fc.forward_chaining(self.current_problems)[2] if x[0] == 'A']
-        print(*predict_reasons, sep='\n')
-        print(*advices, sep="\n")
+        problems = fc.forward_chaining(self.current_problems)[2]
+        period, status, predict_reasons, advices = '', '', [], []
+        for x in problems:
+            if x[0] == 'P':
+                period = x
+            elif x[0] == 'A':
+                advices.append(x)
+            elif x[:2] == 'ST':
+                status = x
+            elif x[:2] == 'RS':
+                predict_reasons.append(x)
+        chatbot_print(f'Chúng tôi đã có thông tin giới tính, chiều cao, cân nặng của con bạn. Theo đánh giá, con bạn đang trong tình trạng {self.facts[status]}')
+        chatbot_print('Một cách chi tiết hơn, chúng tôi thấy con bạn có thể đang gặp các tình trạng sau:')
+        for reason in predict_reasons:
+            chatbot_print2(f'\t{self.facts[reason]}')
+        return period, status, predict_reasons, advices, problems
     def confirm(self):
         pass
 
