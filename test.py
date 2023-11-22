25c9860dde4ad4ea17f75643d32e5f64b9f09d04
henrytran1604 <quanghuy1604nd@gmail.com>
2023-11-22 17:29:21 +0700

    22/11/2023

--- test.py
+++ test.py
@@ -1,22 +1,4 @@
-import re
-f = open('tmp.txt', 'r')
-out = open('out.txt', 'w')
-def convert(s):
-    try:
-        tmp = int(s[-2:0])
-        print(tmp)
-        return s
-    except:
-        return s[:-1] + '0' + s[-1]
-cnt1, cnt2 = 65, 86
-for line in f.readlines():
-    s = re.split(r',|\t', line.strip())
-    print(s)
-    for i in range(len(s) - 1):
-        x = s[i].strip()
-        out.write(f"('{x.strip()}', '{s[-1].strip()}', 'RU{cnt2}'),\n")
-        cnt1 += 1
-    cnt2 += 1
+
 
 #f = open('inference.txt')
 #out = open('out.txt', 'w')
